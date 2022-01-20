e203e3ce065add523927fec8448dca0c0d1867c0
Arjun G Krishna <arjunradhakrishnan52@gmail.com>
2022-01-20 20:49:05 +0530

    Update recursive.c

--- exp6/recursive.c
+++ exp6/recursive.c
@@ -3,63 +3,74 @@
 #include<stdlib.h>
 #include<ctype.h>
 char input[10];
-int i=0,error=0;
-void E();
-void T();
-void Eprime();
-void Tprime();
-void F();
-void main()
+int i = 0, error = 0;
+void E ();
+void T ();
+void Eprime ();
+void Tprime ();
+void F ();
+void
+main ()
 {
-printf("Enter an expression:\n");
-scanf("%s",input);
-E();
-if(strlen(input)==i&&error==0)
-printf("Accepted");
-else
-printf("Rejected");
+  printf ("Enter an expression:\n");
+  scanf ("%s", input);
+  E ();
+  if (strlen (input) == i && error == 0)
+    printf ("Accepted");
+  else
+    printf ("Rejected");
 }
-void E()
+
+void
+E ()
 {
-T();
-Eprime();
+  T ();
+  Eprime ();
 }
-void Eprime()
+
+void
+Eprime ()
 {
-if(input[i]=='+')
-{
-i++;
-T();
-Eprime();
+  if (input[i] == '+')
+    {
+      i++;
+      T ();
+      Eprime ();
+    }
 }
-}
-void T()
+
+void
+T ()
 {
-F();
-Tprime();
+  F ();
+  Tprime ();
 }
-void Tprime()
+
+void
+Tprime ()
 {
-if(input[i]=='*')
-{
-i++;
-F();
-Tprime();
+  if (input[i] == '*')
+    {
+      i++;
+      F ();
+      Tprime ();
+    }
 }
-}
-void F()
+
+void
+F ()
 {
-if(isalpha(input[i]))
-i++;
-else if(input[i]=='(')
-{
-i++;
-E();
-if(input[i]==')')
-i++;
-else
-error=1;
-}
-else
-error=1;
+  if (isalpha (input[i]))
+    i++;
+  else if (input[i] == '(')
+    {
+      i++;
+      E ();
+      if (input[i] == ')')
+	i++;
+      else
+	error = 1;
+    }
+  else
+    error = 1;
 }
